6d6e6987559692a2cffc82c600f473e71765880c
georgedimitriadis <gdimitri@hotmail.com>
2019-05-22 12:20:28 +0100

    added fwd bwd buttons and edit text to slider.py

--- slider.py
+++ slider.py
@@ -20,9 +20,29 @@ class MainWindow(bt.BasicTransform):
 
         self.input_label = QtWidgets.QLabel()
 
+        self.button_fwd = QtWidgets.QPushButton('FWD')
+        self.button_fwd.setIcon(self.style().standardIcon(QtWidgets.QStyle.SP_ArrowForward))
+        self.button_fwd.clicked.connect(self._on_button_forwards)
+
+        self.button_bwd = QtWidgets.QPushButton('BWD')
+        self.button_bwd.setIcon(self.style().standardIcon(QtWidgets.QStyle.SP_ArrowBack))
+        self.button_bwd.clicked.connect(self._on_button_backwards)
+
+        self.edit_input = QtWidgets.QLineEdit()
+        self.edit_input.returnPressed.connect(self._on_new_position)
+        self.edit_input.textEdited.connect(self._on_position_edited)
+
+        self.layout_buttons = QtWidgets.QHBoxLayout()
+
+        self.layout_buttons.addWidget(self.button_bwd)
+        self.layout_buttons.addWidget(self.button_fwd)
+        self.layout_buttons.addWidget(self.edit_input)
+
         self.layout_labels.addWidget(self.input_label)
         self.layout_labels.addWidget(self.label_transfrom_func_name)
         self.layout_labels.addWidget(self.label_output)
+
+        self.layout_window.addLayout(self.layout_buttons)
         self.layout_window.addWidget(self.input_widget)
 
     def _set_input_label(self):
@@ -42,6 +62,7 @@ class MainWindow(bt.BasicTransform):
         self.input_var_value = self.input_widget.value()
         self.repl_globals[self.input_var_name] = self.input_var_value
         self._set_input_label()
+        self.edit_input.setText(str(self.input_var_value))
 
     def _get_input_var_value(self):
         if self.update_input_from_repl:
@@ -53,6 +74,20 @@ class MainWindow(bt.BasicTransform):
                 print('Variable {} not defined in the REPL'.format(self.input_var_name))
                 self.close()
 
+    def _on_button_forwards(self):
+        self.input_widget.setValue(self.input_widget.value() + self.input_widget.singleStep())
+
+    def _on_button_backwards(self):
+        self.input_widget.setValue(self.input_widget.value() - self.input_widget.singleStep())
+
+    def _on_new_position(self):
+        value = int(self.edit_input.text())
+        if value < self.input_widget.minimum() or value > self.input_widget.maximum():
+            return
+        self.input_widget.setValue(value)
+
+    def _on_position_edited(self):
+        pass
 
 def connect_repl_var(repl_globals, input_var_name, function_name, output_var_name, function_args_name=None,
                      slider_limits=None):
